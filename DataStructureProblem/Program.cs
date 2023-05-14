c70c8d21f9fc8eec8f9da9f65c22ad8f51251151
Rahulgautama <rahul8874950727@gmail.com>
2023-05-14 17:57:10 +0530

    Create Simple Linked list and store 56,30,70

---
+++ DataStructureProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructureProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(30);
+            linkedList.Add(56);
+            linkedList.Add(70);
+
+            linkedList.Display();
+
+        }
+    }
+}
