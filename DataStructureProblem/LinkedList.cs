480f509bea258120802885f721106ea864b8faa6
Rahulgautama <rahul8874950727@gmail.com>
2023-05-15 00:49:44 +0530

    Create Linked append list and store 56,30,70

---
+++ DataStructureProblem/LinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureProblem
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public Node GetLastNode(LinkedList linkedList)
+        {
+            Node temp = linkedList.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void Append(LinkedList linkedList, int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (linkedList.head == null)
+            {
+                linkedList.head = new_node;
+                return;
+            }
+            Node lastNode = GetLastNode(linkedList);
+            lastNode.next = new_node;
+        }
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted into Linked List ", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureProblem/Program.cs
+++ DataStructureProblem/Program.cs
@@ -7,16 +7,15 @@ namespace DataStructureProblem
     {
         static void Main(string[] args)
         {
-            
-            LinkedList<int> linkedList = new LinkedList<int>();
-            linkedList.AddFirst(56);
-            linkedList.AddFirst(30);
-            linkedList.AddFirst(70);
+            LinkedList linkedList = new LinkedList();
+            //linkedList.Add(56);
+            //linkedList.Add(30);
+            //linkedList.Add(70);
 
-            foreach (var dataList in linkedList)
-            {
-                Console.WriteLine(dataList);
-            }
+            linkedList.Append(linkedList,56);
+            linkedList.Append(linkedList,30);
+            linkedList.Append(linkedList,70);
+            linkedList.Display();
 
         }
     }
